bb5e41d81838c60c9218f4abe6c656a23dd9db0a
edinson0810 <lache081015@gmail.com>
2024-08-10 18:19:01 -0500

    java

--- js/apps.js
+++ js/apps.js
@@ -6,8 +6,8 @@ const input= document.querySelector(".nav__input");
 
 link.forEach(element => {
     element.addEventListener("click", function(event){
-if(input.checked){
-input.checked= false
-}
-}) 
+    if(input.checked){
+        input.checked= false
+    }
+    }) 
 });
